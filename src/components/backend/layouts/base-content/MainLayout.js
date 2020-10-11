1ef84a8033b7cb6790bb3734f0531458f155748b
Md. Shahin Howlader <Shahin>
2020-10-11 12:57:40 +0200

    page mastering done

--- src/components/backend/layouts/base-content/MainLayout.js
+++ src/components/backend/layouts/base-content/MainLayout.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Sidebar from "../sidebar/Sidebar";
+import PageHeader from "../page-header/PageHeader";
+import Footer from "../footer/Footer";
 
 const MainLayout = (props) => {
     return (
         <>
-            <div id="page-container" className="sidebar-o sidebar-dark enable-page-overlay side-scroll page-header-fixed">
-                {props.children && props.children}
+            <div id="page-container"
+                 className="sidebar-o sidebar-dark enable-page-overlay side-scroll page-header-fixed">
+                <Sidebar/>
+                <PageHeader/>
+                <main id="main-container">
+                    {props.children && props.children}
+                </main>
+                <Footer />
+
             </div>
 
         </>
