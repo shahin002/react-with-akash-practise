6a4b5822c5c6ed82c5d3236068c16b82bab397c7
Md. Shahin Howlader <Shahin>
2020-10-14 19:46:37 +0200

    post list and create file creation done

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import LoginContainer from "../views/backend/auth/LoginContainer";
 import RegisterContainer from "../views/backend/auth/RegisterContainer";
 import DashboardContainer from "../views/backend/dashboard/DashboardContainer";
+import PostListContainer from "../views/backend/post/PostListContainer";
+import PostAddContainer from "../views/backend/post/PostAddContainer";
 
 const routes = [
     {
@@ -19,6 +21,20 @@ const routes = [
         path: "/dashboard",
         name: "Dashboard Component",
         component: DashboardContainer,
+        exact: true,
+    },
+
+    /*** Posts Routes ***/
+    {
+        path: "/posts",
+        name: "Post Component",
+        component: PostListContainer,
+        exact: true,
+    },
+    {
+        path: "/posts/create",
+        name: "Post Create Component",
+        component: PostAddContainer,
         exact: true,
     },
 ]
---
+++ src/views/backend/post/PostAddContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostCreate from "../../../components/backend/post/PostCreate";
+
+const PostAddContainer = () => {
+    return (
+        <>
+            <PostCreate />
+        </>
+    );
+};
+
+export default PostAddContainer;
